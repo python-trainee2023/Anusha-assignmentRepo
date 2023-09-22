61fb88a0e88b017bcf2d29af9968bfa6b8b24c72
KC <anusha.kc@cotiviti.com>
2023-09-22 21:05:04 +0545

    new file added as function.py

---
+++ function.py
@@ -0,0 +1,26 @@
+#function is for code reusability
+
+# def test():
+#     print("hello")
+# python_test()
+
+# def python_test(n):
+#     square=n*2
+#     # return square
+    
+# output=python_test(5) #functioncall
+# print(output)
+
+# def python_test(a,b):
+#     #docstrings """
+    
+#     print(a)
+#     print(b)
+#     sum=a+b
+#     return sum
+# output=python_test(3,5)
+# print(output)
+
+
+
+
