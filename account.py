deeebe97e91499611f92d043c0adc34f05e82b7b
KC <anusha.kc@cotiviti.com>
2023-09-29 10:29:11 +0545

    Assignment_9_29 bank package added

---
+++ account.py
@@ -0,0 +1,32 @@
+class Account:
+    def __init__(self,account_number,holder_name,balance):
+        self.account_number=account_number
+        self.holder_name=holder_name
+        self.balance=balance
+
+    def display_info(self):
+        print(f"Account Number: {self.account_number}")
+        print(f"Holder Name: {self.holder_name}")
+        print(f"balance: {self.balance:.2f}")
+
+class SavingsAccount(Account):
+    def __int__(self,account_number,holder_name,balance,interest_rate):
+        Account.__init__(account_number,holder_name,balance)
+        self.interest_rate=interest_rate
+
+    def calc_interest(self):
+        return self.balance*(self.interest_rate/100)
+
+class CheckingAccount(Account):
+    def __init__(self,account_number,holder_name,balance,overdraft_limit):
+        super().__init__(account_number,holder_name,balance)
+        self.overdraft_limit=overdraft_limit
+
+    def withdraw(self,amount):
+        if amount<=self.balance+self.overdraft_limit:
+            self.balance-=amount
+            return True
+            print("withdrawal was successful")
+        else:
+            return False
+            print("withdrawal was not successful")
---
+++ banking_sys.py
@@ -0,0 +1,18 @@
+from Bank.account import SavingsAccount,CheckingAccount
+from Bank.transaction import deposit,withdraw
+
+savings_account=SavingsAccount("NIBIL","Anusha",1000)
+checking_account=CheckingAccount("CA678","Bipana",10000,5.1)
+
+savings_account.display_info()
+checking_account.display_info()
+
+deposit(savings_account,600)
+withdraw(checking_account,500)
+
+print("\nUpdated information: ")
+savings_account.display_info()
+checking_account.display_info()
+
+interest=savings_account.calc_interest()
+print(f"Interest earned: Rs{interest:.2f}")
